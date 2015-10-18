ab409e08ec839ed5b68fa12554aaa3d7c313f2b9
wizzard94 <wizzard94@users.noreply.github.com>
2015-10-18 17:29:39 -0300

    Un par de cagadas que otras, pero nada del otro mundo. - Error manager para hacer mas legible los errores - Cargar listas - Cargar padron - Intento de Comando Abrir - Comando ingresar se defiende bien TO-DO: - De todo, muchos estan comentados (//TODO:) - Ver como va a ser el ciclo de votacion - Donde se almacenan los votos para posterior conteo

--- tp1.c
+++ tp1.c
@@ -13,95 +13,300 @@
 #include "tda/vector_dinamico.h"
 
 #define VECTOR_TAM_INICIAL 50
+#define PUESTOS_A_VOTAR 5 // Cantidad de puestos a votar + 2 (Por ID y Nombre de la lista)
 
-typedef struct maquina_votacion{
-    cola_t* cola;
-    hash_t* padron;
-    vector_t* listas;
-    pila_t* ciclo;
-} maquina_votacion_t;
-
-// Struct para almacenar como dato en el Hash
+/* Struct para almacenar los votantes en el padron y la cola */
 typedef struct votante {
     char* documento_tipo;
     char* documento_numero;
     bool voto_realizado;
 } votante_t;
 
-/*
-1- While de comandos de entrada por stdin
-2- Cargar archivos en lista y hash
-*/
+/* Codigos de error escritos user-friendly*/
+typedef enum {
+    LECTURA,
+    MESA_ABIERTA,
+    MESA_CERRADA,
+    NUMERO_NEGATIVO,
+    VOTO_REALIZADO,
+    NO_ENPADRONADO,
+    NO_VOTANTES,
+    NO_DESHACER,
+    FALTA_VOTAR,
+    OTRO,
+    COLA_NO_VACIA
+} error_code;
 
+/* Posibles estados de la maquina de votar */
+typedef enum {
+    CERRADA,
+    ABIERTA,
+    VOTACION
+} maquina_estado;
+
+typedef struct maquina_votacion{
+    // Estado actual de la maquina
+    maquina_estado estado;
+    // Cola de votantes esperando
+    cola_t* cola;
+    // Padron de votantes que deben votar
+    hash_t* padron;
+    // Listas habilitadas para ser votadas
+    lista_t* listas;
+    // Vector para almacenar los votos
+    vector_t* votos;
+    // Ciclo donde se guardan los datos mientras un votante este votando
+    pila_t* ciclo;
+    // Cargo que se esta votando actualmente (de estar votandose)
+    int votando_cargo;
+} maquina_votacion_t;
+
+/* Imprime codigo de error */
+void error_manager(error_code code) {
+    /*
+    ERROR1: si hubo un error en la lectura de los archivos (o archivos inexistentes).
+    ERROR2: si la mesa ya estaba previamente abierta
+    ERROR3: si la mesa no está abierta.
+    ERROR4: si el número de DNI es menor o igual a 0.
+    ERROR5: si la persona ya votó antes.
+    ERROR6: si el número y tipo de documento no está en el archivo padrón.csv.
+    ERROR8: en caso de que no existan operaciones para deshacer.
+    ERROR7: en caso de que no existan votantes en espera.
+    ERROR9: en caso de que no se hayan elegido todas las categorías.
+    ERROR10: en cualquier otro caso no contemplado.
+    ERROR11: En caso de que aún queden votantes ingresados sin emitir su voto
+    */
+    printf("ERROR%d\n", code+1);
+}
+
+/*
+ Abre el archivo de listas y crea una lista_t por cada lista de candidatos (cada linea del archivo es una lista).
+ Carga cada lista_t creada dentro de la lista de maquina->listas
+ Post: Devuelve false en caso de no haber modificado maquina->listas.
+*/
+bool cargar_listas(maquina_votacion_t* maquina, char* listas) {
+    // TODO: Cerrar archivo antes de devolver False en algun error
+    FILE* f = fopen(listas,"r");
+	if(!f)
+    {
+        error_manager(LECTURA);
+        return false;
+    }
+
+	char* linea = leer_linea(f);
+	while(linea)
+	{
+	    // TODO: si fila es NULL?
+		fila_csv_t* fila = parsear_linea_csv(linea, PUESTOS_A_VOTAR);
+
+		lista_t* lista_candidatos = lista_crear();
+		if(!lista_candidatos)
+        { /* TODO: si falla? */ }
+
+        // TODO: Si devuelve false en algun insertar?
+        for(int i=0;i<PUESTOS_A_VOTAR;i++)
+            lista_insertar_ultimo(lista_candidatos, obtener_columna(fila, i))
+
+        // TODO: Si devuelve false al insertar?
+        lista_insertar_ultimo(maquina->listas, lista_candidatos);
+
+        destruir_fila_csv(fila, true);
+        lista_candidatos = NULL;
+        linea = NULL;
+
+        // Leer proxima linea
+        linea = leer_linea(f);
+	}
+	free(linea);
+	fclose(f);
+
+	return true;
+}
+
+/*
+ Abre el archivo de padron y crea un votante_t por cada votante (cada linea del archivo es un votante).
+ Carga cada votante_t creado dentro del hash de maquina->padron
+ Post: Devuelve false en caso de no haber modificado maquina->padron.
+*/
+bool cargar_padron(maquina_votacion_t* maquina, char* padron) {
+    // TODO: Cerrar archivo antes de devolver False en algun error
+    FILE* f = fopen(padron,"r");
+	if(!f)
+    {
+        error_manager(LECTURA);
+        return false;
+    }
+
+	char* linea = leer_linea(f);
+
+	// Cantidad de columnas en el padron (Tipo y Numero de documento)
+	size_t cantidad_Columnas = 2;
+	while(linea)
+	{
+	    // TODO: si fila es NULL?
+		fila_csv_t* fila = parsear_linea_csv(linea, cantidad_Columnas);
+
+        votante_t* votante = malloc(sizeof(votante_t));
+		if(!votante)
+        { /* TODO: si falla? */ }
+
+        char* documento_tipo = obtener_columna(fila, 0);
+        char* documento_numero = obtener_columna(fila, 1);
+
+        votante->documento_numero = documento_numero;
+        votante->documento_tipo = documento_tipo;
+        votante->voto_realizado = false;
+
+        // TODO: Fallo al guardar
+        hash_guardar(maquina->padron,documento_numero, votante);
+
+        destruir_fila_csv(fila, true);
+        votante = NULL;
+        linea = NULL;
+
+        // Leer proxima linea
+        linea = leer_linea(f);
+	}
+	free(linea);
+	fclose(f);
+
+	return true;
+}
 
 /*
  Abrir archivos .csv y almacenar la informacion en TDAs
  Lista para las listas.
  Hash para el padron.
 */
-bool comando_abrir(char* listas, char* padron) {
-    printf("Comando abrir ejecutado: %s , %s \n", listas, padron);
+bool comando_abrir(maquina_votacion_t* maquina, char* listas, char* padron) {
+    if(!listas || !padron)
+    {
+        error_manager(LECTURA);
+        return false;
+    }
+
+    if(maquina->estado >= ABIERTA)
+    {
+        error_manager(MESA_ABIERTA);
+        return false;
+    }
+
+    // TODO: Si falla al crear lista.
+    maquina->listas = lista_crear();
+    // TODO: Si falla al crear lista.
+    maquina->padron = hash_crear(free);
+
+    cargar_listas(maquina, listas);
+    cargar_padron(maquina, padron);
+
+	maquina->estado = ABIERTA;
+
     return true;
 }
 
-/*
- Crear y encolar struct votante_t.
-*/
-void comando_ingresar(char* documento_tipo, char* documento_numero){
+/* Crear y encolar struct votante_t. */
+bool comando_ingresar(maquina_votacion_t* maquina, char* documento_tipo, char* documento_numero) {
     printf("Comando ingresar ejecutado: %s , %s \n", documento_tipo, documento_numero);
+
+    // Error handling
+    if(maquina->estado == CERRADA)              { error_manager(MESA_CERRADA);      return false; }
+    if(!documento_tipo || !documento_numero)    { error_manager(OTRO);              return false; }
+    if(strtol(documento_numero, NULL, 10) < 0)  { error_manager(NUMERO_NEGATIVO);   return false; }
+
+    votante_t* votante = malloc(sizeof(votante_t));
+
+    if(!votante) return error_manager(OTRO);
+
+    votante->documento_numero = documento_numero;
+    votante->documento_tipo = documento_tipo;
+
+    if( cola_encolar(maquina->cola, votante) )
+        return true;
+
+    error_manager(OTRO);
+    return false;
 }
 
 /*
  Desencolar y realizar validacion del Documento tipo/numero del votante y no haber votado
  Crear pila para ciclo de votacion actual
 */
-void comando_votar_inicio(){
+bool comando_votar_inicio(maquina_votacion_t* maquina){
     printf("Comando votar inicio ejecutado \n");
+
+    // Error handling
+    if(maquina->estado == CERRADA)      { error_manager(MESA_CERRADA);  return false; }
+    if(maquina->estado == VOTACION)     { error_manager(OTRO);          return false; }
+    if(cola_esta_vacia(maquina->cola))  { error_manager(NO_VOTANTES);   return false; }
+
+    votante_t* votante_espera = cola_desencolar(maquina->cola);
+    if(!votante) { error_manager(OTRO); return false; }
+
+    bool enpadronado = hash_pertenece(maquina->padron, votante_espera->documento_numero);
+    if(!enpadronado)
+    {
+            error_manager(NO_ENPADRONADO);
+            free(votante_espera);
+            return false;
+    }
+
+    votante_t* votante_padron = hash_obtener(maquina->padron, votante_espera->documento_numero);
+
+    if(votante_padron->voto_realizado)
+    {
+        error_manager(VOTO_REALIZADO);
+        free(votante_espera);
+        return false;
+    }
+
+    pila_t* ciclo = pila_crear();
+    if(!ciclo)
+    {
+        error_manager(OTRO);
+        return false;
+    }
+
+    maquina->estado = VOTACION;
+    maquina->ciclo = ciclo;
+    maquina->votando_cargo = 1;
+
+    // TODO: Mostrar menu de votacion
 }
 
 /*
  Almacenar en la pila de votacion el partido votado.
  Validar que exista ciclo de votacion.
 */
-void comando_votar_idPartido(char* id){
+void comando_votar_idPartido(char* id) {
     printf("Comando votar idPartido ejecutado \n");
 }
 
-/*
- Retroceder la pila de votacion
-*/
-void comando_votar_deshacer(){
+/* Retroceder la pila de votacion */
+void comando_votar_deshacer() {
     printf("Comando votar deshacer ejecutado \n");
 }
 
 
-/*
- Cerrar ciclo de votacion y procesar resultados
-*/
-void comando_votar_fin(){
+/* Cerrar ciclo de votacion y procesar resultados */
+void comando_votar_fin() {
     printf("Comando votar fin ejecutado \n");
 }
 
-/*
- Procesar todos los votos y volcal resultados
-*/
-void comando_cerrar(){
+/* Procesar todos los votos y volcal resultados */
+void comando_cerrar() {
     printf("Comando cerrar ejecutado \n");
 }
 
-/*
- Formatear y mostrar menu de votacion
-*/
-void mostrar_listado(int cargo);
+/* Formatear y mostrar menu de votacion */
+void mostrar_listado(maquina_votacion_t* maquina) {
+}
 
 
-/*
- Leer entrada e intentar formatear comandos
-*/
-void leer_entrada() {
+/* Leer entrada e intentar formatear comandos */
+void leer_entrada(maquina_votacion_t* maquina) {
 	bool terminar = false;
 
-	// Valor maximo de columnas
+	// Valor maximo de parametros para cualquier comando
 	size_t columnas = 3;
 
 	char* cmd_abrir       = "abrir";
@@ -129,10 +334,10 @@ void leer_entrada() {
         cmd_param2    = obtener_columna(fila, 2);
 
         if( strcmp(cmd_ingresado, cmd_abrir)==0 )
-            comando_abrir(cmd_param1, cmd_param2);
+            comando_abrir(maquina, cmd_param1, cmd_param2);
 
         else if( strcmp(cmd_ingresado, cmd_ingresar)==0 )
-            comando_ingresar(cmd_param1, cmd_param2);
+            comando_ingresar(maquina, cmd_param1, cmd_param2);
 
         else if( strcmp(cmd_ingresado, cmd_votar)==0 )
         {
@@ -165,49 +370,23 @@ void leer_entrada() {
 */
 int main() {
     maquina_votacion_t* maquina = malloc(sizeof(maquina_votacion_t));
-    if(!maquina) return NULL;
+    if(!maquina) return 1;
 
     cola_t* cola = cola_crear();
     if(!cola) { free(maquina); return NULL; }
 
-    /*hash_t* padron = hash_crear(free);
-    if(!padron)
-    {
-        free(maquina);
-        cola_destruir(cola, NULL);
-        return NULL;
-    }*/
-
-    pila_t* ciclo = pila_crear();
-    if(!ciclo)
-    {
-        free(maquina);
-        cola_destruir(cola, NULL);
-        //hash_destruir(padron);
-        return NULL;
-    }
-
-    vector_t* listas = vector_crear(VECTOR_TAM_INICIAL);
-    if(!listas)
-    {
-        free(maquina);
-        cola_destruir(cola, NULL);
-        //hash_destruir(padron);
-        pila_destruir(ciclo);
-        return NULL;
-    }
-
-    maquina->ciclo = ciclo;
+    maquina->estado = CERRADA;
     maquina->cola = cola;
-    maquina->listas = listas;
-    //maquina->padron = padron;
+    maquina->ciclo = NULL;
+    maquina->listas = NULL;
+    maquina->padron = NULL;
+    maquina->votando_cargo = 0;
+
 
     leer_entrada(maquina);
 
     free(maquina);
     cola_destruir(cola, free);
-    //hash_destruir(padron);
-    pila_destruir(ciclo);
 
 	return 0;
 }
